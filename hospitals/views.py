fb791c55f1fbca4fe6b72de9ee1da5ccc2d60e9f
karth <karthiknaveen22022002@gmail.com>
2021-05-16 13:08:02 +0530

    added tables to the all the services tab

--- hospitals/views.py
+++ hospitals/views.py
@@ -1,16 +1,10 @@
 from django.shortcuts import render
 from django.http import HttpResponse 
-# Create your views here.
-
-
 from django.db.models.query import QuerySet
-from django.shortcuts import render
 from .models import *
 from rest_framework import viewsets
 from .serializers import *
-#    Hospital.objects.values('name_of_hospital','total_icu_beds','total_o2_beds','total_normal_beds','address').distinct()
-# this line of code will create a list of dictonaries
-# order_by will order according to alphabetical order of the passed element
+
 
 class StateViewSet(viewsets.ModelViewSet):
     queryset = State.objects.all()
@@ -44,52 +38,51 @@ class StaffViewSet(viewsets.ModelViewSet):
     queryset = StaffData.objects.all()
     serializer_class = StaffSerializer
 
+
+
 def index(request):
 	states = State.objects.all()
 	w = {}
 	for i in states:
-		print(i.state)
+		# print(i.state)
 		w[i.state] = []
 		dist = District.objects.filter(state_id=i)
 		#print(dist.district)
 		for j in dist:
 			w[i.state].append(j.district)
-	print(w)
+	# print(w)
+	return render(request,'index.html',{'w':w})
 
-	return render(request,'hospitals/index.html',{'w':w})
+
 def results(request):
     pass
 
-def hospitals(request):
-    obj = Hospital.objects.values('name_of_hospital','total_icu_beds','total_o2_beds','total_normal_beds','address').distinct()
-    print(obj)
-    return render(request,'hospitals/bedinfo.html',{'obj':obj})
 
-# def list_of_hospitals(request,id1):
-#     obj = Hospital.objects.filter(district=id1)
-#     w={}
-#     for i in obj:
-#         w[i.name_of_hospital] = i.address
-    
-#     return render(request,'hospitals/hospitals.html',{'w':w})
-# services we have to proovide the hospitals ,oxygen cylinders,ambulances and medical store details
+
 def services(request,id1,id2):
 	states = State.objects.filter(state=id1)
-	#print(states[0].state)
+	# print(type(states))
+	# here states is an object
+	# we can not use them directly it is will print like <QuerySet [<State: State object (1)>]>
+	# and this object object is of type class like <class 'django.db.models.query.QuerySet'>
+	# y beacause we are filtering  this from the model class 
+
+	# print(states[0])
 	dist = District.objects.filter(district=id2)
 	#print(dist[0].district)
+
+
 	lst = []
 	lst.append(states[0].state)
 	lst.append(dist[0].district)
 	print(lst)
 
 
+
 	hosp = Hospital.objects.filter(district_id=dist[0])
 	ambu = Ambulances.objects.filter(district_id=dist[0])
 	oxygen = oxygen_cylinders.objects.filter(district_id=dist[0])
 	medicine = Medicines.objects.filter(district_id=dist[0])
-
-
 	hosp_list = []
 	ambulances_list = []
 	oxygen_cylinder_list = []
@@ -98,19 +91,54 @@ def services(request,id1,id2):
 
 	for i in hosp:
 		w = {
-		'name' : i.name_of_hospital
+		'name_of_hospital' : i.name_of_hospital
 		}
 		hosp_list.append(w)
-	 
+		
+		
+	for i in ambu:
+		w = {
+		'vehicle_no_of_the_ambulance' : i.vehicle_no_of_the_ambulance
+		}
+		ambulances_list.append(w)
+		
+		
+	for i in oxygen:
+		w = {
+		'name_of_the_oxygen_dealerame' : i.name_of_the_oxygen_dealer
+		}
+		oxygen_cylinder_list.append(w)
+		
+		
+	for i in medicine:
+		w = {
+		'name_of_the_medical_store' : i.name_of_the_medical_store
+		}
+		medical_store_list.append(w)
 
-	return render(request,'services.html',{'hosp_list':hosp_list, 'lst':lst})    
 
-def hosp_list(request,name1,st,dt):
+
+	services={
+		'hosp_list':hosp_list,
+		'ambulances_list':ambulances_list,
+  "oxygen_cylinder_list":oxygen_cylinder_list,
+  "medical_store_list":medical_store_list, 
+		'lst':lst
+		}
+
+		
+	return render(request,'services.html',services)    
+
+
+
+
+
+def hospital_list(request,name1,st,dt):
 	states = State.objects.filter(state=st)
 	dist = District.objects.filter(district=dt)
 	hosp = Hospital.objects.filter(district_id=dist[0],name_of_hospital=name1)
 	data = []
-	print(hosp)
+
 	for i in hosp:
 		 data.append(i.name_of_hospital)
 		 data.append(i.total_icu_beds)
@@ -119,5 +147,52 @@ def hosp_list(request,name1,st,dt):
 		 data.append(i.total_normal_beds)
 		 data.append(i.contact_number_of_the_hospital)
 
-	return render(request,'hospitals/hosp_list.html',{'data':data})
+	return render(request,'hospitals/hospitalList.html',{'data':data})
 
+
+
+	
+def ambulance_list(request,name1,st,dt):
+	states = State.objects.filter(state=st)
+	dist = District.objects.filter(district=dt)
+	ambulance = Ambulances.objects.filter(district_id=dist[0],vehicle_no_of_the_ambulance=name1)
+	data = []
+
+	for i in ambulance:
+		 data.append(i.name_of_the_ambulance_driver)
+		 data.append(i.vehicle_no_of_the_ambulance)
+		 data.append(i.contact_no_of_ambulance_driver)
+
+
+	return render(request,'ambulances/ambulanceList.html',{'data':data})
+
+
+def oxygen_cylinder_list(request,name1,st,dt):
+	states = State.objects.filter(state=st)
+	dist = District.objects.filter(district=dt)
+	oxygen = oxygen_cylinders.objects.filter(district_id=dist[0],name_of_the_oxygen_dealer=name1)
+	data = []
+
+	for i in oxygen:
+		 data.append(i.name_of_the_oxygen_dealer)
+		 data.append(i.contact_no_of_the_oxygen_dealer)
+
+
+
+	return render(request,'oxygenCylinders/oxygenCylinderList.html',{'data':data})	
+
+
+
+def medical_store_list(request,name1,st,dt):
+	states = State.objects.filter(state=st)
+	dist = District.objects.filter(district=dt)
+	medicine = Medicines.objects.filter(district_id=dist[0],name_of_the_medical_store=name1)
+	data = []
+
+	for i in medicine:
+		 data.append(i.name_of_the_medical_store)
+		 data.append(i.name_of_the_shop_owner)
+		 data.append(i.contact_no_of_the_medical_shop_owner)
+
+
+	return render(request,'medicalStores/medicalStoreList.html',{'data':data})	
--- wearethere/urls.py
+++ wearethere/urls.py
@@ -19,16 +19,20 @@ urlpatterns = [
     path('api-auth/',include('rest_framework.urls',
         namespace='rest_framework')),
     path('',include(router.urls)),
+
+
     path('index/', views.index, name='index'),
     path('admin/', admin.site.urls),
     #path('hospitals/',views.hospitals, name='hospitals'),
 
 
     # url(r'list_of_hospitals/(?P<id1>[A-Za-z-]+)$',views.list_of_hospitals, name='list_of_hospitals'),
+    
     url(r'services/(?P<id1>[A-Za-z- ]+)/(?P<id2>[A-Za-z- ]+)$',views.services, name='services'),
-    url(r'hosp_list/(?P<name1>[A-Za-z]+)/(?P<st>[A-Za-z- ]+)/(?P<dt>[A-Za-z- ]+)$', views.hosp_list,name='hosp_list'),
 
-    # testing purpose
-    path('test/',views.services, name='hospitals'),
+    url(r'hospital_list/(?P<name1>[A-Za-z]+)/(?P<st>[A-Za-z- ]+)/(?P<dt>[A-Za-z- ]+)$', views.hospital_list,name='hosp_list'),
+    url(r'ambulance_list/(?P<name1>[A-Za-z]+)/(?P<st>[A-Za-z- ]+)/(?P<dt>[A-Za-z- ]+)$', views.ambulance_list,name='ambulance_list'),
+    url(r'oxygen_cylinder_list/(?P<name1>[A-Za-z]+)/(?P<st>[A-Za-z- ]+)/(?P<dt>[A-Za-z- ]+)$', views.oxygen_cylinder_list,name='oxygen_cylinder_list'),
+    url(r'medical_store_lisst/(?P<name1>[A-Za-z]+)/(?P<st>[A-Za-z- ]+)/(?P<dt>[A-Za-z- ]+)$', views.medical_store_list,name='medical_store_list'),
 
 ]
